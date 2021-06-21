68ecca0704f44d60b2f331e22374f1f7ad29b2c6
Halil-Muhammad <hmuhammad0397@gmail.com>
2021-06-21 22:43:57 +0930

    first commit on review1 branch

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -6,6 +6,8 @@ public class BranchingPractice {
         //added .gitignore with .idea , target folder and .iml file
         //made initial commit by keeping track of all files
         //shared project on github
+
+        //reviewing on new branch
     }
 }
 
