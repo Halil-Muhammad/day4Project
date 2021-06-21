7f0eb994302b7e566dd7fb021f4bf8caaf002065
Halil-Muhammad <hmuhammad0397@gmail.com>
2021-06-21 22:40:44 +0930

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,11 @@
 public class BranchingPractice {
     public static void main(String[] args) {
 
+        //created new project
+        //enable version control
+        //added .gitignore with .idea , target folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
     }
 }
 
